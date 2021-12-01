d0365694067b16e5b8886aaec18abe0ad7782fc5
Soobin <ssbin93@gmail.com>
2021-12-02 08:27:04 +1000

    Refactor e2e test file

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -22,7 +22,7 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it('/ (GET) 200', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
@@ -30,14 +30,14 @@ describe('AppController (e2e)', () => {
   });
 
   describe('/movies', () => {
-    it('GET', () => {
+    it('GET 200', () => {
       return request(app.getHttpServer())
         .get('/movies')
         .expect(200)
         .expect([]);
     });
 
-    it('POST', () => {
+    it('POST 201', () => {
       return request(app.getHttpServer())
         .post('/movies')
         .send({
@@ -48,7 +48,19 @@ describe('AppController (e2e)', () => {
         .expect(201);
     });
 
-    it('DELETE', () => {
+    it('POST 400', () => {
+      return request(app.getHttpServer())
+        .post('/movies')
+        .send({
+          title: 'Test Movie',
+          year: 2020,
+          genres: ['test', 'action'],
+          hacked: 'by Soobin'
+        })
+        .expect(400);
+    });
+
+    it('DELETE 404', () => {
       return request(app.getHttpServer())
         .delete('/movies')
         .expect(404);
@@ -68,8 +80,17 @@ describe('AppController (e2e)', () => {
         .expect(404);
     });
 
-    it.todo('DELETE');
+    it('PATCH 200', () => {
+      return request(app.getHttpServer())
+        .patch('/movies/1')
+        .send({title: 'Patched Movie'})
+        .expect(200);
+    });
 
-    it.todo('PATCH');
+    it('DELETE 200', () => {
+      return request(app.getHttpServer())
+        .delete('/movies/1')
+        .expect(200);
+    });
   });
 });
